f0979391caab80c60443cdd1dc32b682460c6eb4
hame <hamed.h.1111@gmail.com>
2015-03-06 18:28:09 +0330

    adfads

--- controllers/default.js
+++ controllers/default.js
@@ -170,7 +170,7 @@ function view_Loggedin() {
                 if (model.check == "checked") {
                     self.res.cookie('mwdcookie', myinfo.ID.toString(), new Date().add('day', 1));
                 }
-                self.view('Login', docs.toString());
+                self.view('Login',docs[1]);
             }
             db.close();
         });
